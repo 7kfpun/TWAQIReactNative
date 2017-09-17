2fe29e44185031cd5c3b7d2d0de6fa6b4f58217d
kf <7kfpun@gmail.com>
2017-09-17 23:51:24 +0800

    Link google admob; update launch screen

--- ios/TWAQI/AppDelegate.m
+++ ios/TWAQI/AppDelegate.m
@@ -24,7 +24,8 @@
                                                       moduleName:@"TWAQI"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  // rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [UIColor colorWithRed:0.72 green:0.88 blue:0.95 alpha:1.0];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
