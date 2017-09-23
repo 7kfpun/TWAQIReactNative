0453ba1b36726affdcc618180532776dd49d66ce
kf <7kfpun@gmail.com>
2017-09-23 13:15:33 +0800

    Use TabNavigator

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ import {
 } from 'react-native';
 
 import { AdMobInterstitial } from 'react-native-admob';
-import { StackNavigator } from 'react-navigation';
+import { TabNavigator } from 'react-navigation';
 
 import Main from './app/views/main';
-// import Help from './app/views/help';
+import Help from './app/views/help';
 import Settings from './app/views/settings';
 
 import { config } from './app/config';
@@ -17,12 +17,24 @@ if (!__DEV__) {
   console.log = () => {};
 }
 
-const App = StackNavigator({
+const App = TabNavigator({
   Main: { screen: Main },
   Settings: { screen: Settings },
-  // Help: { screen: Help },
+  Help: { screen: Help },
 }, {
-  mode: 'modal',
+  headerMode: 'none',
+  swipeEnabled: true,
+  animationEnabled: true,
+  tabBarOptions: {
+    activeTintColor: '#29B6F6',
+    labelStyle: {
+      fontSize: 13,
+      paddingBottom: 4,
+    },
+    style: {
+      backgroundColor: 'white',
+    },
+  },
 });
 
 console.ignoredYellowBox = [
