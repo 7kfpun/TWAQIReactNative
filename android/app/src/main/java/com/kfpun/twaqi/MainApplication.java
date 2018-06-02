2f4563b6ce5d0046483fb54f3c1d50f6c0d3d8b2
kf <7kfpun@gmail.com>
2018-06-02 16:42:36 +0800

    Update android files

--- android/app/src/main/java/com/kfpun/twaqi/MainApplication.java
+++ android/app/src/main/java/com/kfpun/twaqi/MainApplication.java
@@ -3,22 +3,31 @@ package com.kfpun.twaqi;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.smixx.fabric.FabricPackage;
-import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
-import com.mustansirzia.fused.FusedLocationPackage;
+import com.reactNativeQuickActions.AppShortcutsPackage;
+import io.fabric.sdk.android.Fabric;
+import com.airbnb.android.react.maps.MapsPackage;
+import fr.greweb.reactnativeviewshot.RNViewShotPackage;
+import com.horcrux.svg.SvgPackage;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.sbugert.rnadmob.RNAdMobPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.airbnb.android.react.maps.MapsPackage;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
+import io.invertase.firebase.RNFirebasePackage;
+
+import com.smixx.fabric.FabricPackage;
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.answers.Answers;
+
+import io.invertase.firebase.admob.RNFirebaseAdMobPackage;
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
+import io.invertase.firebase.crash.RNFirebaseCrashPackage;
+import io.invertase.firebase.perf.RNFirebasePerformancePackage;
+import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
+
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
-import com.crashlytics.android.Crashlytics;
-import com.crashlytics.android.answers.Answers;
-import io.fabric.sdk.android.Fabric;
 
 import java.util.Arrays;
 import java.util.List;
@@ -35,15 +44,27 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new AppShortcutsPackage(),
+          new RNFirebaseAdMobPackage(),
+          new RNFirebaseAnalyticsPackage(),
+          new RNFirebaseCrashPackage(),
+          new RNFirebasePerformancePackage(),
+          new RNFirebaseCrashlyticsPackage(),
           new FabricPackage(),
-          new RNI18nPackage(),
-          new FusedLocationPackage(),
+          new MapsPackage(),
+          new RNViewShotPackage(),
+          new SvgPackage(),
           new ReactNativeOneSignalPackage(),
           new RNDeviceInfo(),
-          new RNAdMobPackage(),
           new VectorIconsPackage(),
-          new MapsPackage()
+          new RNI18nPackage(),
+          new RNFirebasePackage()
       );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
     }
   };
 
@@ -55,8 +76,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-
     Fabric.with(this, new Crashlytics(), new Answers());
+    SoLoader.init(this, /* native exopackage */ false);
   }
 }
